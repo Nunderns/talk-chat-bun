87b7ac68103a32866c67007bb68037caad4a0be2
Henri Okayama <67149267+Nunderns@users.noreply.github.com>
2026-02-15 08:03:59 -0300

    feat: socket (#6)

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,17 @@
 import 'dotenv/config';
 import app from "./src/app";
 import { connectDB } from "./src/config/database";
+import { createServer } from "http";
+import { initializeSocket } from './src/utils/socket';
 
 const PORT = process.env.PORT || 3000
 
+const httpServer = createServer(app);
+
+initializeSocket(httpServer);
+
 connectDB().then(() => {
-  app.listen(PORT, () => {
+  httpServer.listen(PORT, () => {
     console.log("Servidor esta funcinonando na porta:", PORT)
   })
 })
--- backend/src/routes/messageRoutes.ts
+++ backend/src/routes/messageRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { protectRoute } from "../middleware/auth";
+import { getMessages } from "../controllers/messageController";
 
 const router = Router();
 
-router.get("/chat/:chatId", protectRoute, getChatMessages);
+router.get("/chat/:chatId", protectRoute, getMessages);
 
 export default router;
