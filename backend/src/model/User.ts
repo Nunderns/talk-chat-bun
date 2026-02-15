bb4126a5b70a664981cd3d838ec8236855024ae7
Henri Okayama <henri.okayama@gmail.com>
2026-02-15 01:08:44 -0300

    feat: socket

--- backend/src/model/User.ts
+++ backend/src/model/User.ts
@@ -1,40 +1,41 @@
 import mongoose, { Schema, type Document } from "mongoose";
 
 export interface IUser extends Document {
-    clerkId: string;
-    name: string;
-    email: string;
-    avatar?: string;
-    createdAt: Date;
-    updatedAt: Date;
+  clerkId: string;
+  name: string;
+  email: string;
+  avatar: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-const UserSchema = new Schema<IUser>({
+const UserSchema = new Schema<IUser>(
+  {
     clerkId: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
-    name: { 
-        type: String, 
-        required: true,
-        trim: true
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    email: { 
-        type: String, 
-        required: true, 
-        unique: true,
-        lowercase: true,
-        trim: true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
     avatar: {
-        type: String,
-        default: "",
-    }
-}, {
+      type: String,
+      default: "",
+    },
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-const User = mongoose.model<IUser>("User", UserSchema);
-
-export default User;
+export const User = mongoose.model("User", UserSchema);
